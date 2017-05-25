ddc8cca8b31eb2ccd229c1a9adb6a5e35762225f
Cédric Pahud <cedric.pahud@he-arc.ch>
2017-05-25 19:18:25 +0200

    modif pour le stop

--- src/airport/com/Avion.java
+++ src/airport/com/Avion.java
@@ -93,6 +93,8 @@ public class Avion implements Runnable {
 		isPaused();
 		listAirArr.add(this);
 		airportFrame.avionInAir(this);
+		Thread.sleep(10);
+		isPaused();
 		
 		land();
 		Thread.sleep(10);
@@ -200,6 +202,8 @@ public class Avion implements Runnable {
 	//Arrive dans l'espace a�rien de l'a�ro-porc.
 		airArr.put(this);
 		airportFrame.avionInAir(this);
+		Thread.sleep(1000);
+		isPaused();
 	//(Demande) Atterissage.
 		tarmacLand.put(this);
 		airArr.remove(this);
